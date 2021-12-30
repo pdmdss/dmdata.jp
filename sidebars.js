661771d27e91c645b078abf470896fd2c2407354
soshi1822 <soshi1822@gmail.com>
2021-12-30 22:22:09 +0900

    add: 試験・訓練報の取り扱いについて
    Signed-off-by: soshi1822 <soshi1822@gmail.com>

--- sidebars.js
+++ sidebars.js
@@ -1,6 +1,6 @@
 module.exports = {
     docs: {
-        'Manual': ['index', 'parameter', 'sample/javascript', 'eew'],
+        'Manual': ['index', 'parameter', 'sample/javascript', 'eew', 'testing'],
         'Reference': [
             'reference/index',
             {
