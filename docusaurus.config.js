baceb6832c39581ee7008f62f6926a330ac178a8
soshi1822 <soshi1822@gmail.com>
2022-05-11 20:47:26 +0900

    change: config で型チェックをするように
    Signed-off-by: soshi1822 <soshi1822@gmail.com>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,76 +1,86 @@
-module.exports = {
+/**
+ * @ts-check
+ */
+
+
+/** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+const themeConfig = {
+    navbar: {
+        title: 'Top',
+        logo: {
+            alt: 'logo',
+            src: 'img/logo.png'
+        },
+        items: [
+            {
+                to: '/reference/',
+                label: 'API Reference',
+                position: 'left'
+            },
+            {
+                to: '/telegrams/',
+                label: '配信データ',
+                position: 'left'
+            },
+            {
+                href: 'https://github.com/pdmdss/doc.dmdata.jp',
+                label: 'GitHub',
+                position: 'right'
+
+            }
+        ]
+    },
+    footer: {
+        style: 'dark',
+        links: [
+            {
+                title: 'Site',
+                items: [
+                    {
+                        label: 'サービストップページ',
+                        href: 'https://dmdata.jp'
+                    },
+                    {
+                        label: 'コントロールパネル',
+                        href: 'https://manager.dmdata.jp/control/'
+                    },
+                    {
+                        label: 'Service status',
+                        href: 'https://dmdata.statuspage.io/'
+                    }
+                ]
+            },
+            {
+                title: 'Social',
+                items: [
+                    {
+                        label: 'Twitter (@pdmdata)',
+                        href: 'https://twitter.com/pdmdata'
+                    },
+                    {
+                        label: 'Discord',
+                        href: 'https://discord.gg/uP6gfgzKnA'
+                    }
+                ]
+            }
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} dmdata.jp`
+    },
+    tableOfContents: {
+        minHeadingLevel: 2,
+        maxHeadingLevel: 6
+    }
+};
+
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
     title: 'DMDATA.JP Document',
     url: 'https://dmdata.jp',
     baseUrl: '/doc/',
     onBrokenLinks: 'throw',
     favicon: 'img/favicon.ico',
-    themeConfig: {
-        navbar: {
-            title: 'Top',
-            logo: {
-                alt: 'logo',
-                src: 'img/logo.png'
-            },
-            items: [
-                {
-                    to: '/reference/',
-                    label: 'API Reference',
-                    position: 'left'
-                },
-                {
-                    to: '/telegrams/',
-                    label: '配信データ',
-                    position: 'left'
-                },
-                {
-                    href: 'https://github.com/pdmdss/doc.dmdata.jp',
-                    label: 'GitHub',
-                    position: 'right'
-
-                }
-            ]
-        },
-        footer: {
-            style: 'dark',
-            links: [
-                {
-                    title: 'Site',
-                    items: [
-                        {
-                            label: 'サービストップページ',
-                            href: 'https://dmdata.jp'
-                        },
-                        {
-                            label: 'コントロールパネル',
-                            href: 'https://manager.dmdata.jp/control/'
-                        },
-                        {
-                            label: 'Service status',
-                            href: 'https://dmdata.statuspage.io/'
-                        }
-                    ]
-                },
-                {
-                    title: 'Social',
-                    items: [
-                        {
-                            label: 'Twitter (@pdmdata)',
-                            href: 'https://twitter.com/pdmdata'
-                        },
-                        {
-                            label: 'Discord',
-                            href: 'https://discord.gg/uP6gfgzKnA'
-                        }
-                    ]
-                }
-            ],
-            copyright: `Copyright © ${new Date().getFullYear()} dmdata.jp`
-        },
-        tableOfContents: {
-            minHeadingLevel: 2,
-            maxHeadingLevel: 6
-        }
-    },
+    themeConfig,
     presets: [
         [
             '@docusaurus/preset-classic',
@@ -86,3 +96,5 @@ module.exports = {
         ]
     ]
 };
+
+module.exports = config;
